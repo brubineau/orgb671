9da07a98036c3ea5f0dfc1a90164a2e05156a729
brubineau <brian.rubineau@mcgill.ca>
2020-04-29 08:54:42 -0400

    Added comments

--- RegressionToMean.R
+++ RegressionToMean.R
@@ -4,11 +4,11 @@
 # NR  O1  XB  O2
 
 thresh <- 1.5
-perf <- rnorm(1000)
+perf <- rnorm(1000) # 1000 observations of performance
 
 # no effect
-clap <- function(kpi,th){
-  return(
+clap <- function(kpi,th){  # how often, after observing at-or-above-positive-threshold behavior
+  return(                  # is positive reinforcement followed by an improvement?
     sum(
       sapply(1:(length(kpi)-1),
              function(i){
@@ -18,8 +18,8 @@ clap <- function(kpi,th){
   )
 }
 
-yell <- function(kpi,th){
-  return(
+yell <- function(kpi,th){ # how often, after observing at-or-below-negative-threshold behavior
+  return(                 # is negative reinforcement followed by an improvement?
     sum(
       sapply(1:(length(kpi)-1),
              function(i){
@@ -31,7 +31,7 @@ yell <- function(kpi,th){
 clap(perf,thresh)
 yell(perf,thresh)
 
-p2 <- rnorm(1000000)
+p2 <- rnorm(1000000) # 1M observations of performance
 clap(p2,thresh)
 yell(p2,thresh)
 clap(p2,1)
@@ -39,14 +39,25 @@ yell(p2,1)
 
 # claps help, yelling harms effect
 delta <- 0.5
-proClap <- function(kpi,th){
+proClap <- function(kpi,th){ # give improved (by delta) performance after positive reinforcement
   return(rnorm(1,mean=as.numeric(kpi > th)*delta))
 }
 
-conYell <- function(kpi,th){
+conYell <- function(kpi,th){ # give reduced (by delta) performance after negative reinforcement
   return(rnorm(1,mean=as.numeric(kpi < (-1*th))*(-1*delta)))
 }
 
-sum(sapply(p2[which(p2>thresh)],function(r){as.numeric(proClap(r,thresh)>=r)}))/sum(as.numeric(p2>thresh))
-sum(sapply(p2[which(p2<(-1*thresh))],function(r){as.numeric(conYell(r,thresh)>=r)}))/sum(as.numeric(p2<(-1*thresh)))
+sum(sapply(p2[which(p2>thresh)], # for each above-threshold performance in p2,
+           function(r){          # give the next (w/improved mean) performance
+             as.numeric(proClap(r,thresh)>=r)
+             }
+           ))/                  # how often is this an improvement over the
+  sum(as.numeric(p2>thresh))    # behavior prompting the positive reinforcement?
+
+sum(sapply(p2[which(p2<(-1*thresh))],  # for each below-threshold performance in p2,
+           function(r){                # give the next (w/lower mean) performance
+             as.numeric(conYell(r,thresh)>=r)
+             }                         # how often is the next performance an
+           ))/                         # improvement over the behavior prompting the
+  sum(as.numeric(p2<(-1*thresh)))      # negative reinforcement?
 
